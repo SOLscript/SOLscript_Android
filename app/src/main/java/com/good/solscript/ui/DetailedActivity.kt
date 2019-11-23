8b30c3b562e81dd2c9e9317857196f969e304d10
seunghee63 <0603yang@naver.com>
2019-11-23 11:17:58 +0900

    Add detailed/category list ui

---
+++ app/src/main/java/com/good/solscript/ui/DetailedActivity.kt
@@ -0,0 +1,13 @@
+package com.good.solscript.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.good.solscript.R
+
+class DetailedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailed)
+    }
+}
