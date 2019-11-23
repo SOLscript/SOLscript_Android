808ebb104754ab88e6877592230d1b45592c5da7
seunghee63 <0603yang@naver.com>
2019-11-24 01:04:20 +0900

    Delete not using import

--- app/src/main/java/com/good/solscript/ui/SubscriptManageFragment.kt
+++ app/src/main/java/com/good/solscript/ui/SubscriptManageFragment.kt
@@ -2,37 +2,23 @@ package com.good.solscript.ui
 
 
 import android.annotation.SuppressLint
-import android.app.usage.UsageStats
-import android.app.usage.UsageStatsManager
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Bundle
-import android.provider.Settings
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.utils.ColorTemplate
 import com.good.solscript.R
 import com.good.solscript.adapter.ManageAdapter
-import com.good.solscript.adapter.UsageListAdapter
 import com.good.solscript.data.ManageData
 import com.good.solscript.util.CustomTemplate
-import com.good.solscript.util.CustomUsageStats
 import kotlinx.android.synthetic.main.fragment_subscript_manage.*
-import java.util.*
-import kotlin.collections.ArrayList
-import com.github.mikephil.charting.data.BarDataSet as BarDataSet
 
 /**
  * A simple [Fragment] subclass.
